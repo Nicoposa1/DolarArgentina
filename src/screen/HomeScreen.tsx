730cd4d08b38818b13af30f1a79ca57efcccae2d
Nicolas Posa <nicoposa57@gmail.com>
2023-10-01 20:20:00 -0300

    test carrousels

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,8 +1,15 @@
-import {View, Text, StyleSheet, StatusBar, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import React from 'react';
 import {AUTH_TOKEN} from '@env';
 import LinearGradient from 'react-native-linear-gradient';
-// import LinearGradient from 'react-native-linear-gradient';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 
 export const HomeScreen = () => {
   const [data, setData] = React.useState<any>(null);
@@ -14,8 +21,9 @@ export const HomeScreen = () => {
   );
   const [arsTransform, setArsTransform] = React.useState<number>(0);
   const [arsTransformed, setArsTransformed] = React.useState<String>('');
-  const [usdTransform, setUsdTransform] = React.useState<number>(0);
+  const [usdTransform, setUsdTransform] = React.useState<number>('');
   const [usdTransformed, setUsdTransformed] = React.useState<String>('');
+  const items = [1, 2, 3, 4, 5, 6]; // Los elementos de tu carrusel
 
   React.useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +38,12 @@ export const HomeScreen = () => {
 
     fetchData();
   }, []);
+
+  const renderItem = ({item}) => (
+    <View style={styles.item}>
+      <Text style={styles.itemText}>{item}</Text>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -70,27 +84,104 @@ export const HomeScreen = () => {
             placeholderTextColor={'gray'}
             keyboardType="numeric"
           />
-          <Text style={styles.resultsText}>USD${usdTransformed}</Text>
+          {usdTransform > 0 && (
+            <Text style={styles.resultsText}>
+              ARS$
+              {usdTransform
+                ? (usdTransform * data?.venta)
+                    .toFixed(2)
+                    .replace(/\d(?=(\d{3})+\.)/g, '$&,')
+                : ''}
+            </Text>
+          )}
         </View>
-        <Text style={styles.subtitle}>Transforma ARS$ a USD$</Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1,
-            backgroundColor: 'white',
-            borderRadius: 10,
-            padding: 10,
-          }}
-          onChangeText={number => {
-            setArsTransform(number ? parseInt(number) : 0);
-          }}
-          value={arsTransform ? arsTransform.toString() : ''}
-          placeholder="ARS$"
-          placeholderTextColor={'gray'}
-          keyboardType="numeric"
-        />
-        <Text style={styles.resultsText}>ARS${arsTransformed}</Text>
+        <View
+          style={[
+            styles.bodyContainer,
+            {
+              height: 100,
+            },
+          ]}>
+          <Text style={[styles.subtitle, {marginTop: 10}]}>
+            Transforma ARS$ a USD$
+          </Text>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: 'gray',
+              borderWidth: 1,
+              backgroundColor: 'white',
+              borderRadius: 10,
+              padding: 10,
+            }}
+            onChangeText={text => {
+              setArsTransform(text ? parseInt(text) : 0);
+            }}
+            value={arsTransform ? arsTransform.toString() : ''}
+            placeholder="ARS$"
+            placeholderTextColor={'gray'}
+            keyboardType="numeric"
+          />
+        </View>
+        {arsTransform > 0 ? (
+          <View
+            style={{
+              height: 40,
+            }}>
+            <Text style={[styles.resultsText]}>
+              USD$
+              {arsTransform
+                ? (arsTransform / data?.venta)
+                    .toFixed(2)
+                    .replace(/\d(?=(\d{3})+\.)/g, '$&,')
+                : ''}
+            </Text>
+          </View>
+        ) : (
+          <View
+            style={{
+              height: 40,
+            }}
+          />
+        )}
+
+        <View style={{
+          marginTop: 20,
+          height: 100,
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <Carousel
+            data={items}
+            renderItem={renderItem}
+            sliderWidth={300}
+            itemWidth={100}
+            loop
+            autoplay
+            autoplayInterval={3000}
+          />
+        </View>
+
+        <ScrollView
+          style={styles.budgetContainer}
+          horizontal={true}
+          // hacer que se haga carrousel infinito
+
+          pagingEnabled={true}>
+          <View style={styles.budget}>
+            <Text style={styles.crrouselSubtitle}>Compra USD Oficial </Text>
+            <Text style={styles.carrouselText}>ARS$ {data?.compra}</Text>
+          </View>
+          <View style={styles.budget}>
+            <Text style={styles.crrouselSubtitle}>Venta USD Oficial </Text>
+            <Text style={styles.carrouselText}>ARS$ {data?.venta}</Text>
+          </View>
+          <View style={styles.budget}>
+            <Text style={styles.crrouselSubtitle}>Compra USD Oficial </Text>
+            <Text style={styles.carrouselText}>ARS$ {data?.compra}</Text>
+          </View>
+        </ScrollView>
       </LinearGradient>
     </View>
   );
@@ -130,5 +221,43 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginLeft: 10,
     color: 'white',
+  },
+  budgetContainer: {
+    marginTop: 20,
+    height: 100,
+    width: '100%',
+  },
+
+  budget: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    width: '50%',
+    alignItems: 'center',
+    height: 80,
+    marginRight: 10,
+    justifyContent: 'center',
+  },
+  carrouselText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#0072ff',
+  },
+  crrouselSubtitle: {
+    fontSize: 15,
+    fontWeight: '700',
+    color: '#0072ff',
+  },
+  item: {
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    height: 100,
+    width: 100,
+  },
+  itemText: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
